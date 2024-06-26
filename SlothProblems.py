49a1685ad341e12cab7d029c6eb637166b632d38
Alessandro Caula <alessandro.caula@3brain.com>
2024-06-26 12:06:28 +0200

    Initial Implementation of sloth problem

--- SlothProblems.py
+++ SlothProblems.py
@@ -1,1 +1,35 @@
-print("New File")
+######################
+# Challenge 25/06/2024
+######################
+
+# Create a function that creates a box based on dimension n.
+#
+# makeBox(5) ➞ [
+#   "#####",
+#   "#   #",
+#   "#   #",
+#   "#   #",
+#   "#####"
+# ] 5x5 box
+
+# makeBox(3) ➞ [
+#   "###",
+#   "# #",
+#   "###"
+# ] 3x3 box
+
+# makeBox(2) ➞ [
+#   "##",
+#   "##"
+# ] 2x2 box
+
+# makeBox(1) ➞ [
+#   "#"
+# ] 1x1 box
+
+def makeBox(n):
+    # Create first and last row
+    first_and_last_row = ""
+    for i in range(n):
+        first_and_last_row + "#"
+    # Create middle row
