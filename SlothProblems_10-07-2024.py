bc23e57ac161c7958150d6f836281e1e8889bac5
Alessandro Caula <alessandro.caula@3brain.com>
2024-07-10 18:03:15 +0200

    New SlothProbletm. To be solved still.

---
+++ SlothProblems_10-07-2024.py
@@ -0,0 +1,9 @@
+# Create a function that takes a list of numbers or strings and returns a list with the items from the original list stored into sub lists. Items of the same value should be in the same sub list.
+#
+# Example:
+#
+#advanced_sort([2, 1, 2, 1]) ➞ [[2, 2], [1, 1]]
+#
+#advanced_sort([5, 4, 5, 5, 4, 3]) ➞ [[5, 5, 5], [4, 4], [3]]
+#
+#advanced_sort(["b", "a", "b", "a", "c"]) ➞ [["b", "b"], ["a", "a"], ["c"]]
