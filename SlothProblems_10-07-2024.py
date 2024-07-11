75f589292edb8e0b6d998a532110bafa363ac8b5
Alessandro Caula <alessandro.caula@3brain.com>
2024-07-11 11:24:24 +0200

    New Finance NoteBook and Solved the SlothProblem.

--- SlothProblems_10-07-2024.py
+++ SlothProblems_10-07-2024.py
@@ -7,3 +7,21 @@
 #advanced_sort([5, 4, 5, 5, 4, 3]) ➞ [[5, 5, 5], [4, 4], [3]]
 #
 #advanced_sort(["b", "a", "b", "a", "c"]) ➞ [["b", "b"], ["a", "a"], ["c"]]
+
+def advanced_sort(arr):
+    # Loop through all the elements in the array.
+    dict = {}
+    for i, el in enumerate(arr):
+        print(i, " ", el)
+        if str(el) not in dict.keys():
+            dict[str(el)] = [el]
+        else:
+            dict[str(el)].append(el)
+    advanced_sorted_array = list(dict.values())
+    return advanced_sorted_array
+
+array_to_sort = [2, 2, 3, 2, 2, 1, 1]
+array_to_sort1 = [2, 1, 2, 1]
+array_to_sort2 = [5, 4, 5, 5, 4, 3]
+array_to_sort3 = ["b", "a", "b", "a", "c"]
+print("Advanced Sorting result: ", advanced_sort(array_to_sort3))
