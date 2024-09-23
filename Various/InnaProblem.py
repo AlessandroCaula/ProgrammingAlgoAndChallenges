0ea7ab7367e7316f3ab4d8a79f9c8ec1ec31e3a1
Alessandro Caula <alessandro.caula@3brain.com>
2024-09-23 18:33:39 +0200

    Small changes. Nothing finished.

--- Various/InnaProblem.py
+++ Various/InnaProblem.py
@@ -1,29 +1,30 @@
-matrix_well = []
-
-for row in range(4):
-    new_row = []
-    for col in range(480):
-        new_row.append(col)
-    matrix_well.append(new_row)
-
-
-print("Rows: ", len(matrix_well), "Col: ", len(matrix_well[0]))
+# Given a matrix M x N extract a window array of m (40) lenght from each of the M rows, shifiting the array every rows.
+# i.e. First Row --> array from 0 to 39. Second Row --> array from 40 to 79. Third array --> from 80 to 119. Etc.
+# [[[0 .. 39], 40 .. 480], 
+#  [0 .. 39, .. [40 .. 79], 80 .. 480], 
+#  [0 .. 79, [80 .. 119], 120 .. 480],
+#  [0 .. 119, [120 .. 159], 160 .. 480]]
 
 
-#for i, row in enumerate(matrix_well):
-#    counter = 0
-#    increased = False
-#    for j, col in enumerate(matrix_well[i]):
-#        initial_val = (i + 1) * 40 * counter 
-#        final_val = ((i + 1) * 40 * counter) + 40
-#        if j >= ((i + 1) * 40 * counter) and j < ((i + 1) * 40 * counter) + 40:
-#            matrix_well[i][j] ="prova"
-#            increased = False
-#        else:
-#            if increased == False:
-#                counter += len(matrix_well)
-#                increased = True
+matrix = [[i for i in range(480)] for j in range(4)]
+len_array = 40
+final_array = []
 
-# for i, row in enumerate(matrix_well):
-#     for j, col in enumerate(matrix_well[i]):
-#         if 
+row = 0
+counter = 0 
+for col in range(len(matrix[0])):
+    if counter < len_array:
+        final_array.append(matrix[row][col])
+        counter += 1
+    else:
+        if row == len(matrix) - 1:
+            row = 0
+            counter = 1
+            final_array.append(matrix[row][col])
+        else:
+            row += 1
+            counter = 1
+            final_array.append(matrix[row][col])
+
+print(final_array)
+print(len(final_array))
