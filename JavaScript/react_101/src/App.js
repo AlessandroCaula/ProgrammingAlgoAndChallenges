9a873192a7b93dc8a1460bd0f63bb8eb2b503fef
Alessandro <alecolin96@gmail.com>
2024-10-05 12:13:26 +0200

    Reorganization and new finished react 101

--- JavaScript/react_101/src/App.js
+++ JavaScript/react_101/src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react'; // Hooks
 import './App.css';
 
 // Most important thing on React. Components. Let's create a new functional component. 
@@ -62,8 +62,14 @@ const App = () => {
 
       <h1>-*-*-*-*-*-*-*-*-*-*-*-*-*-</h1>
 
-      {/*Let's do the count for using the react State*/}
+      {/*Let's do the count for using the react useState Hook*/}
       <CounterComponent/>
+
+      <h1>-*-*-*-*-*-*-*-*-*-*-*-*-*-</h1>
+      
+      <h1> useEffect, message on page reloads </h1>  
+      {/* Let's call the component for the useEffect Hook */}
+      <UseEffectHook/>
 
       <h1>-*-*-*-*-*-*-*-*-*-*-*-*-*-</h1>
 
@@ -77,12 +83,28 @@ const App = () => {
 // Let's Create a counter Component. 
 const CounterComponent = () => {
   // concept of array destructuring. That is how state works. 
-  const [counter, setCounter] = useState(0); // Calling the useState as a function. Whenever you call something as a function and you use the "use" in React is called hook.
+  const [counter, setCounter] = useState(0); // Calling the useState as a function (this is a Hook). Whenever you call something as a function and you use the "use" in React is called hook.
   // The first part in the square brackets is going to be name of that state "counter". The second part is the Setter function "set" + state_name ("counter").
   // Inside the useState you put the initial value, and this case will be 0.
-
   // Event is an action that can be triggered as a result of a user action or some kind of a system generated event. I.e. a mouse click or a button press. 
   
+  // !!IMPORTANT rule: NEVER modify the state (counter) manually. This is not just a variable, it is a state. Therefore, it can only be changed by the Setter Function "setCounter".
+  // Here we can use the useEffect Hook in order to modify the counter (strictly using the "setCouner") at reaload, to 100.
+  useEffect(() => {
+    setCounter(100);
+  }, []);
+  // The problem now is that every time you click on the + or - button, the useEffect reset the counter to 100. It happens as soon as the value increases. 
+  // The second parameter of the useEffect hooks, comes in handy. Which is called the dependecy array "[]". With this, the counter value is only modified at start.
+  // That's because, when the dependency array is empty, the code inside the use effect parameter function (the code that set the counter to 100), is going to only happen
+  // at the initial load of the component. 
+  // But if we put some variable inside the "[]" then, the useEffect(parameter function) code is going to update every time the variable inside the "[]" array changes.
+  // So what we can do is something like this.
+  useEffect(() => {
+    alert('You have changed the counter to: ' + counter);
+  }, [counter]);
+  // !!! This is not a very nice user experience, but this is done in order to see that you can call some code whenever something happens in your react application. By simply
+  // providing the variable inside the dependency array "[]". And whenever this variable changes, then you can call the code inside the useEffect parameter-function. 
+
   return (
     <>
     {/*In order to make the button do something we have to use the State*/}
@@ -96,6 +118,15 @@ const CounterComponent = () => {
     <button onClick={() => setCounter((prevCounter) => prevCounter + 1)}>+</button>
     </>
   )
+}
+
+const UseEffectHook = () => {
+  // The useEffect Hook will be called in a different way in respect to the useState Hook.
+  // We call it as a function and it accept another function as the first parameter. 
+  // useEffect allows us to do  something on sime kind of an event.
+  useEffect(() => {
+    alert('Reload, useEffect action'); // this code here will run as soon as the page loads.
+  })
 }
 
 
