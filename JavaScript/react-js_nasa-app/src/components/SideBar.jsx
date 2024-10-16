e6d93a4f07679bf1f01c4b3d4d5f2d132923af0d
Alessandro Caula <alessandro.caula@3brain.com>
2024-10-16 18:03:28 +0200

    New parts for the React.js Nasa project

--- JavaScript/react-js_nasa-app/src/components/SideBar.jsx
+++ JavaScript/react-js_nasa-app/src/components/SideBar.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
 export default function SideBar() {
-  return (
-    <div>
-			<h2>The Brutal Martian Land</h2>
-			<div>
-				<p>Description</p>
-				<p><i>Loren ipsum</i></p>
+	return (
+		<div className='sidebar'>
+			<div className='bgOverlay'></div>
+			<div className='sidebarContents'>
+				<h2>The Brutal Martian Land</h2>
+				<div>
+					<p>Description</p>
+					<p><i>Loren ipsum</i></p>
+				</div>
+				<button>
+					<i className="fa-solid fa-right-long"></i>
+				</button>
 			</div>
-    </div>
-  )
+		</div>
+	)
 }
