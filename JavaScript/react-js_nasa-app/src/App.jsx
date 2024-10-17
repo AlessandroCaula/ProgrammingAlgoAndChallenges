ca589725680c8b01a80cf9ef008df1b4c57c0f7d
Alessandro <alecolin96@gmail.com>
2024-10-17 23:46:55 +0200

    New implementation for the React.js NASA projec.

--- JavaScript/react-js_nasa-app/src/App.jsx
+++ JavaScript/react-js_nasa-app/src/App.jsx
@@ -1,16 +1,32 @@
 import Footer from "./components/Footer"
 import SideBar from "./components/SideBar"
 import Main from "./components/Main"
+import { useState } from "react"
 // Main Functional component. The highest hierarchical compoenent. The one that is rendered from the index.html.
 // The index.html renders the "root" in the Main.jsx which set the root as this App.jsx component. 
 // All the others components (i.e. Footer, Main, SideBar, etc.) are rendered here, in the App.jsx component. 
 function App() {
+  // Using a state variable in order to make the page reactive. In this case the default value will be false.
+  const [showModel, setShowModel] = useState(false);
+
+  // Create a function for dealing with the click on the (i) button, in th footer page, which click should show the SideBar with the description of the image.
+  // This function will be then taken and passed to the footer as a prop.
+  // We want then to pass the same function to the sideBar, so that when the button of the sideBar is clicked, the SideBar hides.
+  function handleToggleModel() {
+    // Call the setter function to set the new state of the showModel, by inverting its previous value.
+    setShowModel(!showModel);
+  }
 
   return (
     <>
       <Main />
-      <SideBar>Text from the SideBar</SideBar>
-      <Footer />
+      {/*Now we can conditionally render the SideBar by using the showModel state. If it is true, we will render the SideBar, by using the &&.*/}
+      {/*!!!! You cannot use regular if statements directly inside the curly braces {} like that. The reason is that JSX expects expressions, not statements, and an if statement is a statement.*/}
+      {showModel && (
+        <SideBar handleToggleModel={handleToggleModel} />
+      )}
+      {/*Pass the handleToggleModal function as a prop to the footer component*/}
+      <Footer handleToggleModel={handleToggleModel} />
     </>
   )
 }
--- JavaScript/react-js_nasa-app/src/components/Footer.jsx
+++ JavaScript/react-js_nasa-app/src/components/Footer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-export default function Footer() {
+export default function Footer(props) {
+
+  // Deconstructuring the props that are coming from the app component.
+  const { handleToggleModel } = props;
+
   return (
     <footer>
       <div className='bgGradient'></div>
@@ -8,7 +12,8 @@ export default function Footer() {
         <h2>Footer: Dall-E Mars generated image</h2>
         <h1>APOD PROJECT</h1>
       </div>
-      <button>
+      {/*Assign the handleToggleModel function to the onClick event of the button. So that when it is clicked, the function handleToggleModel is executer, changes the showModel state and the sideBar is shown.*/}
+      <button onClick={handleToggleModel}>
         <i className="fa-solid fa-circle-info"></i>
       </button>
     </footer>
