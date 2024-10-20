a43a360f82c1c94caf9ffb187708f409a09293b2
Alessandro Caula <alecolin96@gmail.com>
2024-10-20 18:39:04 +0200

    Re-organization of folders.

--- JavaScript/next-js_crash_course/app/api/users/route.js
+++
@@ -1,5 +0,0 @@
-// http://localhost:3000/api/users
-
-export async function GET(request) {
-    return new Response('Hello, Next.js!')
-}
--- JavaScript/next-js_crash_course/app/posts/loading.js
+++
@@ -1,6 +0,0 @@
-const Loading = () => {
-    // You can add any UI inside loading, including a Skeleton. 
-    return <_LoadingSkeleton /> // A spinner, or an example of the page, when the page is loading. 
-}
-
-export default Loading;
