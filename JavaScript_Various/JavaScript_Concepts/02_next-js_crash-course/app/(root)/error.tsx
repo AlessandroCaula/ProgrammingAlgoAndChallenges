ceef9a1283035cd80a55fcb3e30348b611925d86
Alessandro Caula <alecolin96@gmail.com>
2025-01-04 13:35:16 +0100

    Error Handling and loading pages

---
+++ JavaScript_Various/JavaScript_Concepts/02_next-js_crash-course/app/(root)/error.tsx
@@ -0,0 +1,32 @@
+// From the Next.js documentation: https://nextjs.org/docs/app/building-your-application/routing/error-handling copy the error.tsx code (component) for displaying the error to the user with a nicer User Interface.
+
+'use client' // Error boundaries must be Client Components
+ 
+import { useEffect } from 'react'
+ 
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+ 
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  )
+}
---
+++ JavaScript_Various/JavaScript_Concepts/02_next-js_crash-course/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// Loading component, that will be shown through all the routes, when the page is reloading.
+
+const loading = () => {
+  return (
+    <div>____ loading _____</div>
+  )
+}
+
+export default loading
