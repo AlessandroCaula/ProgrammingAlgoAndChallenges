f5423ecaf7bc51812b90c03e4bf22dc6f2985e41
AlessandroCaula <alecaula96@gmail.com>
2025-02-01 22:39:13 +0100

    New form for adding the new participant.

--- JavaScript_Various/Expense_Tracker/script.js
+++ JavaScript_Various/Expense_Tracker/script.js
@@ -1,15 +1,12 @@
-// Add the function ad event listener to toggle the visibility of the Expense Form. That will be shown only on the button click.
-
 // Wait until the DOM is fully loaded before running the script
 document.addEventListener("DOMContentLoaded", function () {
   
+  // Add the function ad event listener to toggle the visibility of the Expense Form. That will be shown only on the button click.
+  // 
   // Select the "Add Expense" button
   const addExpenseBtn = document.querySelector(".add-expense-btn");
-
   // Select the expense form that needs to be shown/hidden
   const expenseForm = document.querySelector(".expense-form");
-
-  // Add a click event listener to the button.
   // AddExpenseBtn is a variable that store a reference to the "Add Expense" button in the HTML
   addExpenseBtn.addEventListener("click", function() {
     // Get the computed style of the expense form
@@ -24,6 +21,12 @@ document.addEventListener("DOMContentLoaded", function () {
       // Otherwise, hide the form
       expenseForm.style.display = "none";
     }
-  })
+  });
+
+  // Select the checkboxes
+  // 
+  // Select the checkboxes element for including or nor including in previous expenses
+  const includePreviousCheckbox = document.getElementById('include-previous');
+  const notIncludePreviousCheckbox = document.getElementById('not-include-previous');
 });
 
