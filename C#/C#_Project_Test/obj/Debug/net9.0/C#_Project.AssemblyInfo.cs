70dfbdadbb5b594607bcca1ba3441e9e20fcfaa7
AlessandroCaula <alecaula96@gmail.com>
2025-02-18 19:44:59 +0100

    New Gitignore rules

--- C#/C#_Project_Test/obj/Debug/net9.0/C#_Project.AssemblyInfo.cs
+++ C#/C#_Project_Test/obj/Debug/net9.0/C#_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe78de211b513b889eaab29661a0d3c519dc9fab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e08e7a6bdff83172fbf3bee8c9e3bae2ebab5446")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
