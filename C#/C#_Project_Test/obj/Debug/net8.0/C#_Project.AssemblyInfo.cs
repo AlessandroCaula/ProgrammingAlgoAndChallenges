b4f3f5cafb02b68c797abb37c5440d071118ba57
Alessandro Caula <alecolin96@gmail.com>
2025-02-18 16:05:58 +0100

    Path updated.

--- C#/C#_Project_Test/obj/Debug/net8.0/C#_Project.AssemblyInfo.cs
+++ C#/C#_Project_Test/obj/Debug/net8.0/C#_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a978b3516ab1df9ed001883edc10430804261128")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
