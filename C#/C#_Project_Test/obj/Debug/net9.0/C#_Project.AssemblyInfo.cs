df7b770ae0f43c822fbe9b688148a75a02770f2d
AlessandroCaula <alecaula96@gmail.com>
2025-02-21 23:17:11 +0100

    Test

--- C#/C#_Project_Test/obj/Debug/net9.0/C#_Project.AssemblyInfo.cs
+++ C#/C#_Project_Test/obj/Debug/net9.0/C#_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e08e7a6bdff83172fbf3bee8c9e3bae2ebab5446")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70dfbdadbb5b594607bcca1ba3441e9e20fcfaa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
