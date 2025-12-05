02e11e3d2360e4e54e4f5fe1558bfc75d9915761
AlessandroCaula <alecaula96@gmail.com>
2025-12-05 20:09:23 +0100

    Day 3 - Lobby - Part 2

--- Advent_of_Code_2025/Day_3/lobby.py
+++ Advent_of_Code_2025/Day_3/lobby.py
@@ -1,11 +1,11 @@
 import argparse
 
-def lobby(input_path: str) -> int:
+def lobby_part1(input_path: str) -> int:
     with open(input_path) as banks:
         tot_output_joltage = 0
         for bank in banks:
             bank = bank.strip()
-            
+
             # Loop through all the numbers of the battery
             tenth_max_idx, unit_max_idx = -1, -1
             # First find the higher tenth
@@ -25,11 +25,42 @@ def lobby(input_path: str) -> int:
                         max_unit = el
                         unit_max_idx = j
 
+            # # --- Solution using python logic
+            # best_first = max(bank[:-1])
+            # i = bank.index(best_first)
+            # best_second = max(bank[i+1:])
+            # return int(best_first + best_second)
+
             max_pack_joltage = int(bank[tenth_max_idx] + bank[unit_max_idx])
             tot_output_joltage += max_pack_joltage
 
         return tot_output_joltage
 
+def lobby_part2(input_path: str) -> int:
+    with open(input_path) as banks:
+        tot_output_joltage = 0
+        for bank in banks:
+            bank = bank.strip()
+            twelve_batteries = ""
+            start = 0
+            for i in range(12)[::-1]:
+                curr_max = -1
+                curr_max_idx = -1
+                # Update the end index
+                end = len(bank) - i
+                # Loop through the start and end range
+                for j in range(start, end):
+                    if int(bank[j]) > curr_max:
+                        curr_max = int(bank[j])
+                        curr_max_idx = j
+                # Save the current maximum
+                twelve_batteries += bank[curr_max_idx]
+                # Updated the new start for the next iteration
+                start = curr_max_idx + 1
+            # print(twelve_batteries)
+            tot_output_joltage += int(twelve_batteries)
+
+    return tot_output_joltage
 
 def main():
     parser = argparse.ArgumentParser()
@@ -37,8 +68,11 @@ def main():
     args = parser.parse_args()
     input_path = args.input
 
-    part_1_res = lobby(input_path)
+    part_1_res = lobby_part1(input_path)
     print("Part 1: ", part_1_res)
+
+    part_2_res = lobby_part2(input_path)
+    print("Part 2: ", part_2_res)
 
 if __name__ == "__main__":
     main()
