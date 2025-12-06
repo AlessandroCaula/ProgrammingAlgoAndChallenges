7e60a829db2610fca544c52e7c73b794c11c32d8
AlessandroCaula <alecaula96@gmail.com>
2025-12-06 12:29:18 +0100

    Day 4 - Printing Department - Part 2

--- Advent_of_Code_2025/Day_4/printing_department.py
+++ Advent_of_Code_2025/Day_4/printing_department.py
@@ -1,6 +1,6 @@
 import argparse
 
-def printing_department(input_path: str) -> int:
+def printing_department_part1(input_path: str) -> int:
     with open(input_path) as f:
         rolls_grid = []
         for line in f:
@@ -37,14 +37,61 @@ def printing_department(input_path: str) -> int:
 
     return rolls_to_remove
 
+def printing_department_part2(input_path: str) -> int:
+    with open(input_path) as f:
+        rolls_grid = []
+        for line in f:
+            line = line.strip()
+            rolls_grid.append(list(line))
+        n_rows = len(rolls_grid)
+        n_cols = len(rolls_grid[0])
+        rolls_to_remove = 0
+
+        keep_removing = True
+        while keep_removing:
+            keep_removing = False
+
+            # Loop through all the positions and check if there are less than 4 paper rolls (@)
+            moves = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
+
+            for r in range(len(rolls_grid)):
+                for c in range(len(rolls_grid[r])):
+                    curr_pos = rolls_grid[r][c]
+                    if curr_pos == ".":
+                        continue
+                    
+                    # Loop through each of the moves
+                    tot_neighbors = 0
+                    for move in moves:
+                        n_r, n_c = r + move[0], c + move[1]
+
+                        # Check if it goes out of the grid
+                        if n_r < 0 or n_c < 0 or n_r >= n_rows or n_c >= n_cols:
+                            continue
+                        
+                        # Count how many papers are around
+                        if rolls_grid[n_r][n_c] == "@":
+                            tot_neighbors += 1               
+
+                    # If there are fewer than 4 papers around, remove the current paper
+                    if tot_neighbors < 4:
+                        rolls_to_remove += 1
+                        rolls_grid[r][c] = "."
+                        keep_removing = True
+
+    return rolls_to_remove
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="Path to input file")
     args = parser.parse_args()
     input_path = args.input
 
-    part1_res = printing_department(input_path)
-    print(part1_res)
+    part1_res = printing_department_part1(input_path)
+    print("Part 1:", part1_res)
+
+    part2_res = printing_department_part2(input_path)
+    print("Part 2:", part2_res)
 
 if __name__ == "__main__":
     main()
