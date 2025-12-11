4e0fe2fa7dfb384795c80af0e1699de4695733da
AlessandroCaula <alecaula96@gmail.com>
2025-12-11 18:57:38 +0100

    Day 8 - Playground - Part 2

--- Advent_of_Code_2025/Day_8/playground.py
+++ Advent_of_Code_2025/Day_8/playground.py
@@ -216,13 +216,82 @@ def playground_part1_DSU(input_path):
 
     return biggest[0] * biggest[1] * biggest[2]
 
+def playground_part2(input_path) -> int:
+    # X, Y, Z
+    circuits = []
+    with open(input_path) as f:
+        for line in f:
+            line = line.strip()
+            circuits.append(line.split(","))
+
+    # Compute the euclidean distance between each point
+    distances = []
+    Distance = namedtuple('Distance', ['circuit_1', 'circuit_2', 'distance'])
+    for i in range(len(circuits)):
+        for j in range(i + 1, len(circuits)):
+            c1 = circuits[i]
+            c2 = circuits[j]
+            dx = int(c1[0]) - int(c2[0])
+            dy = int(c1[1]) - int(c2[1])
+            dz = int(c1[2]) - int(c2[2])
+            euclidean_distance = math.sqrt(dx*dx + dy*dy + dz*dz)
+            curr_dist = Distance(i, j, euclidean_distance)
+            distances.append(curr_dist)
+    
+    # Sort the distances
+    sorted_distances = sorted(distances, key=lambda x: x.distance)
+    
+    final_junctions = 0
+    i = 0
+    circuit_clusters = []
+    while i < len(sorted_distances):
+        c1 = sorted_distances[i].circuit_1
+        c2 = sorted_distances[i].circuit_2
+        
+        already_placed = False
+        cl_c1 = -1
+        cl_c2 = -1
+        for z, cl in enumerate(circuit_clusters):
+            if c1 in cl and c2 in cl:
+                already_placed = True
+                break
+            elif c1 in cl and c2 not in cl:
+                cl_c1 = z
+            elif c2 in cl and c1 not in cl:
+                cl_c2 = z
+        
+        if not already_placed:
+            if cl_c1 == -1 and cl_c2 == -1:
+                # Add new cluster
+                circuit_clusters.append({c1, c2})
+            elif cl_c1 != -1 and cl_c2 == -1:
+                # Add circuit 2 into cluster with circuit 1
+                circuit_clusters[cl_c1].add(c2)
+            elif cl_c2 != -1 and cl_c1 == -1:
+                # Add circuit 1 into cluster with circuit 2
+                circuit_clusters[cl_c2].add(c1)
+            elif cl_c1 != -1 and cl_c2 != -1:
+                # Merge two clusters
+                merged = circuit_clusters[cl_c1] | circuit_clusters[cl_c2]
+                for idx in sorted([cl_c1, cl_c2], reverse=True):
+                    del circuit_clusters[idx]
+                circuit_clusters.append(merged)
+
+        # Check if the clusters have been joined together to 1 single big cluster
+        if len(circuit_clusters) == 1 and len(circuit_clusters[0]) == len(circuits):
+            return int(circuits[c1][0]) * int(circuits[c2][0])
+        
+        i += 1
+
+    return -1
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="Path to input file")
     args = parser.parse_args()
     input_path = args.input
 
-    # input_path = "/Users/alessandrocaula/Documents/Devs/Git-Repos/ProgrammingAlgoAndChallenges/Advent_of_Code_2025/Day_8/input.txt"
+    # input_path = "/Users/alessandrocaula/Documents/Devs/Git-Repos/ProgrammingAlgoAndChallenges/Advent_of_Code_2025/Day_8/input_test.txt"
     
     part_1_res = playground_part1(input_path)
     print("Part 1: ", part_1_res)
@@ -230,8 +299,8 @@ def main():
     part_1_res = playground_part1_DSU(input_path)
     print("Part 1 - DSU: ", part_1_res)
 
-    # part_2_res = laboratories_part2(input_path)
-    # print("Part 2: ", part_2_res)
+    part_2_res = playground_part2(input_path)
+    print("Part 2: ", part_2_res)
 
 if __name__ == "__main__":
     main()
