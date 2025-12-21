f1adde74660dd8509f971c2a3aaee7b7bfff1e6d
AlessandroCaula <alecaula96@gmail.com>
2025-12-21 18:00:35 +0100

    Day 11 - Reactor - Part 1

---
+++ Advent_of_Code_2025/Day_11/reactor.py
@@ -0,0 +1,66 @@
+import argparse
+
+
+def reactor_part1(input_path: str):
+    starting_devices = {}
+    dev_point_to = {}
+    with open(input_path) as f:
+        i = 0
+        for line in f:
+            curr_dev_point_to = []
+            line = line.strip()
+            start_device, point_to = line.split(":")
+            starting_devices[start_device.strip()] = i
+            dev_point_to[i] = [d.strip()
+                               for d in point_to.strip().split(" ")]
+            i += 1
+
+    # Find the starting point, which is the 'you'
+    starting_point_idx = starting_devices['you']
+
+    # From the starting point build all the possible path that will reach to 'out'
+    queue = [(el, {el}) for el in dev_point_to[starting_point_idx]]
+    # visited = [el for el in dev_point_to[starting_point_idx]]
+
+    # While the queue is not empty, loop through all the devices connected to the current one
+    paths_count = 0
+    while len(queue) != 0:
+        curr_dev, visited_in_path = queue.pop()
+
+        # Check the current device where it brings to and, if is has not been yest visited, then add it to queue and visited nodes
+        curr_dev_idx = starting_devices[curr_dev]
+        dev_to = dev_point_to[curr_dev_idx]
+
+        # Loop through all the dev_to
+        for device in dev_to:
+            # If the device is equal to 'out', we have reached a way out, and we increment the path_count
+            if device == 'out':
+                paths_count += 1
+            else:
+                # Check if the device has not been visited yet. Add it to the queue and visited devices list
+                if device not in visited_in_path:
+                    # Copy path for new branch
+                    new_path = visited_in_path.copy()
+                    new_path.add(device)
+                    queue.append((device, new_path))
+
+    return paths_count
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="Path to input file")
+    args = parser.parse_args()
+    input_path = args.input
+
+    # input_path = "/Users/alessandrocaula/Documents/Devs/Git-Repos/ProgrammingAlgoAndChallenges/Advent_of_Code_2025/Day_11/input_test.txt"
+
+    part_1_res = reactor_part1(input_path)
+    print("Part 1: ", part_1_res)
+
+    # part_2_res = reactor_part2(input_path)
+    # print("Part 2: ", part_2_res)
+
+
+if __name__ == "__main__":
+    main()
